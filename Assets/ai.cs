856f11ec85a81979d0ba1a8e518eda44884952b9
lukas <luky_f_@hotmail.com>
2013-12-28 14:30:43 +0100

    prefabs erstellt, leben und energiekapsel eingebunden, vogel dreht sich bug behoben, bienenschwarm erstellt, 2. bewegliches obstacle

---
+++ Assets/ai.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ai : MonoBehaviour {
+	
+	Vector3 speed;
+	bool itemBounceUp = true;
+	int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		speed = new Vector3(Random.Range(-0.2f,0.2f),0.0f,0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		
+		if(itemBounceUp)
+			this.transform.position = this.transform.position + speed;
+		else
+			this.transform.position = this.transform.position - speed;
+		
+		count++;
+		if(count>100){
+			count=0;
+			itemBounceUp = !itemBounceUp;
+			if(itemBounceUp)
+				speed = new Vector3(Random.Range(-0.5f,0.5f),Random.Range(-0.5f,0.5f),Random.Range(-0.5f,0.5f));
+		}
+			
+
+	}
+	
+	
+	void itembounce () {
+		while(true)
+			StartCoroutine(waiting(1.0f));
+		}
+	
+	IEnumerator waiting(float t){
+		yield return new WaitForSeconds(t);
+		itemBounceUp = !itemBounceUp;
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
